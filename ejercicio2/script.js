af1aef912ed35a99a7f5da6df375015f57e1bb1e
ori0nis <oihanegdcl@gmail.com>
2024-08-14 22:14:15 +0200

    started

--- ejercicio2/script.js
+++ ejercicio2/script.js
@@ -2,11 +2,9 @@
 
 2.2 Inserta dinamicamente en un html un div que contenga una p con javascript.
 
-2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un 
-	loop con javascript.
+2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.
 
-2.4 Inserta dinamicamente con javascript en un html una p con el 
-	texto 'Soy dinámico!'.
+2.4 Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
 
 2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
 
@@ -16,8 +14,10 @@ const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
 
 2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
 
-2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. 
-	Recuerda que no solo puedes insertar elementos con .appendChild.
+2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. Recuerda que no solo puedes insertar elementos con .appendChild.
 
-2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase 
-	.fn-insert-here */
+2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here */
+
+
+
+// 2.1
